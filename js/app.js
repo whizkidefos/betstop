b041e83e90984f7439d4259879644ce0de9d6bf8
Efosa Igbinehi <liova2273@gmail.com>
2017-12-08 09:51:19 +0100

    added the mobile navigation

--- js/app.js
+++ js/app.js
@@ -8,20 +8,20 @@
     $('select').material_select();
     $('input#input_text').characterCounter();
     $('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year,
-    today: 'Today',
-    clear: 'Clear',
-    close: 'Ok',
-    closeOnSelect: false // Close upon selecting a date,
+        selectMonths: true,
+        selectYears: 15,
+        today: 'Today',
+        clear: 'Clear',
+        close: 'Ok',
+        closeOnSelect: false
   });
     $('.tap-target').tapTarget('open');
     $('.tap-target').tapTarget('close');
     $('.button-collapse').sideNav({
-      menuWidth: 300, // Default is 300
-      edge: 'right', // Choose the horizontal origin
-      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-      draggable: true // Choose whether you can drag to open on touch screens
+        menuWidth: 300,
+        edge: 'right',
+        closeOnClick: true,
+        draggable: true
     });
    
       
